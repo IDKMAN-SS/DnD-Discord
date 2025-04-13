cd8773c3439cd5c0b50a681f224eecce2c618a2f
smithsm16 <smithsm16@etsu.edu>
2025-04-12 20:29:26 -0400

    Update character_management.py
    updated model error

--- api/apihandlers/character_management.py
+++ api/apihandlers/character_management.py
@@ -1,14 +1,13 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.database import get_db
-from database.models import CharacterModel
-from character_model import Character
+from database.models import Character
 import logging
 
 logger = logging.getLogger("uvicorn.error")
 
 #create character
-def create_character(character: CharacterModel, db: Session):
+def create_character(character: Character, db: Session):
     logger.debug(f"Creating character: {character.name}")
     existing = db.query(Character).filter(Character.name == character.name).first()
     if existing:
@@ -28,7 +27,7 @@ def get_character(name: str, db: Session):
     return char
 
 #update character
-def update_character(name: str, character_data: CharacterModel, db: Session):
+def update_character(name: str, character_data: Character, db: Session):
     logger.debug(f"Updating character: {name}")
     char = db.query(Character).filter(Character.name == name).first()
     if not char:
